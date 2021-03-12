e39f136c398d9ac99c92b63560d6ab0405acf7d5
风驭羿 <760699178@qq.com>
2021-03-12 16:10:32 +0800

    project_user

--- kraft-contract-user-server/src/test/java/com/windsoft/user/ProjectUserTest.java
+++ kraft-contract-user-server/src/test/java/com/windsoft/user/ProjectUserTest.java
@@ -1,6 +1,8 @@
 package com.windsoft.user;
 
+import com.windsoft.kraft.contract.mybatis.domain.Project;
 import com.windsoft.kraft.contract.server.user.UserServerApplication;
+import com.windsoft.kraft.contract.server.user.mapper.ProjectUserMapper;
 import com.windsoft.kraft.contract.server.user.service.ProjectUserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,11 +10,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = UserServerApplication.class)
 public class ProjectUserTest {
     @Autowired
     ProjectUserService projectUserService;
+
+    @Autowired
+    ProjectUserMapper projectUserMapper;
 
     @Test
     public void addTest(){
@@ -22,5 +29,11 @@ public class ProjectUserTest {
     @Test
     public void deleteTest(){
         projectUserService.delete(2L,5L);
+    }
+
+    @Test
+    public void test(){
+        List<Project> projects = projectUserMapper.selectByUserID(1L);
+        projects.forEach(item -> System.out.println(item));
     }
 }
