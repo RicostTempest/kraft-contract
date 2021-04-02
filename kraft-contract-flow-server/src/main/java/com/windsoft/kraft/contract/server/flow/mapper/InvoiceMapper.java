a0ea57dfb6cb17aac56288d066fa1375183ecccc
风驭羿 <760699178@qq.com>
2021-04-02 15:10:35 +0800

    开始报销服务的开发

--- kraft-contract-flow-server/src/main/java/com/windsoft/kraft/contract/server/flow/mapper/InvoiceMapper.java
+++
@@ -1,7 +0,0 @@
-package com.windsoft.kraft.contract.server.flow.mapper;
-
-import com.windsoft.kraft.contract.mybatis.domain.Invoice;
-import com.windsoft.kraft.contract.mybatis.mapper.BaseMapper;
-
-public interface InvoiceMapper extends BaseMapper<Invoice> {
-}
--- kraft-contract-mybatis/src/main/java/com/windsoft/kraft/contract/mybatis/domain/Invoice.java
+++ kraft-contract-mybatis/src/main/java/com/windsoft/kraft/contract/mybatis/domain/Invoice.java
@@ -19,8 +19,8 @@ public class Invoice extends BaseEntity {
     private String name;
 
     @Column(name = "invoice_date")
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private Date invoiceDate;
 
     @Column(name = "resource_id")
